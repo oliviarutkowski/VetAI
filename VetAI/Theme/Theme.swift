1d16d17a82713be17ad0bb83c06a8fb8eb5b833b
oliviarutkowski <137834771+oliviarutkowski@users.noreply.github.com>
2025-08-29 06:40:22 -0600

    Refactor HomeView layout and add greeting

--- VetAI/Theme/Theme.swift
+++ VetAI/Theme/Theme.swift
@@ -35,6 +35,7 @@ struct Spacing {
     static let sm: CGFloat = 8
     static let md: CGFloat = 16
     static let lg: CGFloat = 24
+    static let xl: CGFloat = 32
     // New naming used by modern components
     static let l: CGFloat = 16
 }
